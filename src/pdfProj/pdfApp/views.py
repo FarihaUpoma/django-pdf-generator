5cb68d03dde3a643b96f8cb3758ea5fcf7caf0c6
FarihaUpoma <moomtaheen@gmail.com>
2018-05-11 03:06:57 +0600

    initial commit

---
+++ src/pdfProj/pdfApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from easy_pdf.views import PDFTemplateView
+
+from easy_pdf.views import PDFTemplateResponseMixin
+from django.views.generic.detail import DetailView
+from django.contrib.auth import get_user_model
+
+from django.conf import settings
+
+
+class HelloPDFView(PDFTemplateView):
+    template_name = 'hello.html'
+
